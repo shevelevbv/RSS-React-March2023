9b029a01f9d2757117f334018a8fedaf65efb371
Bronislav Shevelev <shevelevbv@hotmail.com>
2023-03-14 15:25:27 -0400

    feat: configure project

---
+++ components/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+export default WrappedApp;
