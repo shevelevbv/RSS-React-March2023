df32827e5c732d448491b14eb1922fce1be2a177
Bronislav Shevelev <shevelevbv@hotmail.com>
2023-03-18 21:21:43 -0400

    feat: style 404 page

--- components/src/pages/NotFound.tsx
+++ components/src/pages/NotFound.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import '../styles/NotFound.scss';
+import {Link} from "react-router-dom";
 
 class NotFound extends React.Component {
   render = (): JSX.Element => {
-    return <div>404 Not Found</div>;
+    return (
+      <main className="main_not-found">
+        <section className="container_not-found">
+          <h1>Oops! The page does not exist</h1>
+          <p>
+            Please use the options in the menu above or go the the <Link to="/">Home</Link> page.
+          </p>
+        </section>
+        <div className="image_not-found">
+          <img src="./src/assets/svg/NotFound.svg" alt="Not found" />
+        </div>
+      </main>
+    );
   };
 }
 
