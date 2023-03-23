b6113365a72efc9015f5359e4b2bf2546d814ad3
Bronislav Shevelev <shevelevbv@hotmail.com>
2023-03-23 13:19:06 -0400

    feat: add action on form submit

---
+++ components/src/components/UserForm.tsx
@@ -0,0 +1,44 @@
+import React, {SyntheticEvent} from 'react';
+import { IUserDetails } from '../pages/UserDetails';
+
+interface IPropsType {
+  addUserCard: (userCard: IUserDetails) => void;
+}
+
+class UserForm extends React.Component<IPropsType> {
+  textInput: React.RefObject<HTMLInputElement>;
+  dateInput: React.RefObject<HTMLInputElement>;
+  constructor(props: IPropsType) {
+    super(props);
+    this.textInput = React.createRef();
+    this.dateInput = React.createRef();
+  }
+
+  validateData = (event: SyntheticEvent): void => {
+    event.preventDefault();
+    if (this.textInput.current && this.dateInput.current) {
+      const userCard = {
+        textInputValue: this.textInput.current.value,
+        dateInputValue: this.dateInput.current.value,
+      };
+      this.props.addUserCard(userCard);
+    }
+  };
+  render = () => {
+    return (
+      <>
+        <form onSubmit={this.validateData}>
+          <label>
+            Username: <input type="text" ref={this.textInput} />
+          </label>
+          <label>
+            Preferred date to deliver your order: <input type="date" ref={this.dateInput} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </>
+    );
+  };
+}
+
+export default UserForm;
--- components/src/pages/UserDetails.tsx
+++ components/src/pages/UserDetails.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import UserForm from '../components/UserForm';
+
+export interface IUserDetails {
+  textInputValue: string;
+  dateInputValue: string;
+}
+
+interface IDetailsState {
+  userCards: Array<IUserDetails>;
+}
 
 class UserDetails extends React.Component {
-  render = () => {
-    return <div>Hey there!</div>;
+  state: IDetailsState = {
+    userCards: [],
+  };
+
+  addUserCard = (userCard: IUserDetails): void => {
+    console.log(this);
+    this.setState({
+      userCards: this.state.userCards.concat(userCard),
+    });
+  };
+  render = (): JSX.Element => {
+    console.log(this.state.userCards);
+    return (
+      <div>
+        <UserForm addUserCard={this.addUserCard} />
+        <ul>
+          {this.state.userCards.map((card) => (
+            <li key={card.textInputValue}>{card.textInputValue}</li>
+          ))}
+        </ul>
+      </div>
+    );
   };
 }
 
